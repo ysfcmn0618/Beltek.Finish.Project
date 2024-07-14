b4f12af688ebb10369c55362235650ba46b42fc3
yusuf <yusufcimen90@windowslive.com>
2024-07-14 09:51:40 +0300

    otomatik student controlu için ekleme silme düzenleme işlemlerinin eklenmesi ve otomatik optimizasyonu

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Beltek.Finish.Project.Database;
+using Beltek.Finish.Project.Models;
+
+namespace Beltek.Finish.Project.Controllers
+{
+    public class StudentsController : Controller
+    {
+       
+        // GET: Students
+        public IActionResult Index()
+        {
+            using (var context=new DbContextStudents())
+            {
+                var lst = context.Students.ToList();
+                return View(lst);
+            }               
+        }
+
+        //// GET: Students/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null || _context.Students == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var student = await _context.Students
+        //        .Include(s => s.Class)
+        //        .FirstOrDefaultAsync(m => m.StudentId == id);
+        //    if (student == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(student);
+        //}
+
+        //// GET: Students/Create
+        //public IActionResult Create()
+        //{
+        //    ViewData["ClassId"] = new SelectList(_context.Classes, "ClassId", "ClassName");
+        //    return View();
+        //}
+
+        //// POST: Students/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("StudentId,StudentName,StudentSurname,StudentNumber,ClassId")] Student student)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(student);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["ClassId"] = new SelectList(_context.Classes, "ClassId", "ClassName", student.ClassId);
+        //    return View(student);
+        //}
+
+        //// GET: Students/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.Students == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var student = await _context.Students.FindAsync(id);
+        //    if (student == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["ClassId"] = new SelectList(_context.Classes, "ClassId", "ClassName", student.ClassId);
+        //    return View(student);
+        //}
+
+        //// POST: Students/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("StudentId,StudentName,StudentSurname,StudentNumber,ClassId")] Student student)
+        //{
+        //    if (id != student.StudentId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(student);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!StudentExists(student.StudentId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["ClassId"] = new SelectList(_context.Classes, "ClassId", "ClassName", student.ClassId);
+        //    return View(student);
+        //}
+
+        //// GET: Students/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.Students == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var student = await _context.Students
+        //        .Include(s => s.Class)
+        //        .FirstOrDefaultAsync(m => m.StudentId == id);
+        //    if (student == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(student);
+        //}
+
+        //// POST: Students/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    if (_context.Students == null)
+        //    {
+        //        return Problem("Entity set 'DbContextStudents.Students'  is null.");
+        //    }
+        //    var student = await _context.Students.FindAsync(id);
+        //    if (student != null)
+        //    {
+        //        _context.Students.Remove(student);
+        //    }
+            
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        //private bool StudentExists(int id)
+        //{
+        //  return (_context.Students?.Any(e => e.StudentId == id)).GetValueOrDefault();
+        //}
+    }
+}
---
+++ Migrations/20240713200608_UnitDb.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beltek.Finish.Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class UnitDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblClasses",
+                columns: table => new
+                {
+                    ClassId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ClassQuota = table.Column<int>(type: "int", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblClasses", x => x.ClassId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblStudents",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    StudentSurname = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false),
+                    StudentNumber = table.Column<int>(type: "int", nullable: false),
+                    ClassId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblStudents", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_tblStudents_tblClasses_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "tblClasses",
+                        principalColumn: "ClassId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblStudents_ClassId",
+                table: "tblStudents",
+                column: "ClassId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblStudents");
+
+            migrationBuilder.DropTable(
+                name: "tblClasses");
+        }
+    }
+}
